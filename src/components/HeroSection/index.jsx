8e8e12232f72f10df3dc46615fa93c76e5715eb8
chathurperera <chathuraperera007@gmail.com>
2023-02-16 17:54:00 +0530

    imported fonts

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./HeroSection.module.scss";
+
+const HeroSection = () => {
+  return <section>
+    
+  </section>;
+};
+
+export default HeroSection;
