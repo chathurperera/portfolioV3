9187c6edd3ceb5c76368ac4e98c0ec9c367fd71a
chathurperera <chathuraperera007@gmail.com>
2023-02-18 21:36:27 +0530

    styles:hero section scroll indicator

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Navbar from "../Navbar";
 import styles from "./HeroSection.module.scss";
+import scrollIndicator from "@/assets/images/scrollIndicator.png";
+import arrowDownIcon from "@/assets/images/arrow-down.png";
 
 const HeroSection = () => {
-  return <section>
-    <Navbar />
-  </section>;
+  return (
+    <section className={styles.heroSection}>
+      <Navbar />
+      <div className={styles.container}>
+        <h1>Full stack developer who loves building things on the internet</h1>
+        <div className={styles.bottom}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            viverra, dui vel fermentum congue, metus turpis fermentum sapien,
+            vitae tempor leo libero vitae nisi. Pellentesque tempor ac
+          </p>
+          <div className={styles.scrollIndicator}>
+            <div className={styles.scrollIndicatorWrapper}>
+              <img src={scrollIndicator} alt="scroll indicator" />
+              <img src={arrowDownIcon} alt="arrow icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
